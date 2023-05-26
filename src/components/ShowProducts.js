3f8970937b79c531ca08f39bb9f734e3b6147357
wilmeraraujo <wilmerweap11@gmail.com>
2023-05-25 23:22:31 -0500

    proyecto crud-react-consumo-api

---
+++ src/components/ShowProducts.js
@@ -0,0 +1,12 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const ShowProducts = () => {
+  return (
+    <div>ShowProducts</div>
+  )
+}
+
+export default ShowProducts
